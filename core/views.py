3a58707bca3a65d9b275ff98739283508622992a
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-08-20 00:55:04 +0530

    feat:Account Settings

--- core/views.py
+++ core/views.py
@@ -37,7 +37,9 @@ def signup(request):
                 )
                 user.save()
                 
-                # Log user in and redirect to settings page 
+                # Log user in and redirect to settings page
+                user_login = auth.authenticate(username = username, password= password)
+                auth.login(request , user_login)
                 
                 # create a profile object for the new user
                 user_model = User.objects.get(username=username)
@@ -68,3 +70,29 @@ def signin(request):
 def Logout(request):
     auth.logout(request)
     return redirect('signin')
+@login_required(login_url='signin')
+def settings(request):
+    user_profile =Profile.objects.get(user=request.user)
+    
+    if request.method == "POST":
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')  
+    return render(request, 'settings.html', context={'user_profile':user_profile})
